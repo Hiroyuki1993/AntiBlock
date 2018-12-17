41a186f336e683f22a20b35326ac8c22b9ecb6c3
Hiroyuki Kuromiya <khiroyuki1993@gmail.com>
2018-12-17 22:51:15 +0900

    add icons

---
+++ app.js
@@ -0,0 +1,380 @@
+new Vue({
+  el: '#app',
+  data () {
+    return {
+      source: [
+        {
+          "text": "貝塚勃起土竜",
+          "anti": true
+        },
+        {
+          "text": "絵に描いた餅を喉に詰まらせた男",
+          "anti": true
+        },
+        {
+          "text": "音の出る業人",
+          "anti": true
+        },
+        {
+          "text": "こどもおじさん",
+          "anti": true
+        },
+        {
+          "text": "無職ムスタファ・ケマル・アタトュルク",
+          "anti": true
+        },
+        {
+          "text": "日本の識字率を100％未満にした男",
+          "anti": true
+        },
+        {
+          "text": "義務教育の限界",
+          "anti": true
+        },
+        {
+          "text": "人生のすべてをまあいいやで誤魔化してきた男",
+          "anti": true
+        },
+        {
+          "text": "つける薬を座薬として服用した男",
+          "anti": true
+        },
+        {
+          "text": "貝塚の闇",
+          "anti": true
+        },
+        {
+          "text": "オフパコ妄想おじさん",
+          "anti": true
+        },
+        {
+          "text": "女性に嫌われる天才",
+          "anti": true
+        },
+        {
+          "text": "セミの抜け殻に精子詰めてそう",
+          "anti": true
+        },
+        {
+          "text": "ダニと週八で交尾してそう",
+          "anti": true
+        },
+        {
+          "text": "ほならね理論提唱者",
+          "anti": true
+        },
+        {
+          "text": "親の金を出会い系につぎ込む無職",
+          "anti": true
+        },
+        {
+          "text": "広島産無職",
+          "anti": true
+        },
+        {
+          "text": "大阪製土竜",
+          "anti": true
+        },
+        {
+          "text": "泉南汚物",
+          "anti": true
+        },
+        {
+          "text": "泉南ゴブリン",
+          "anti": true
+        },
+        {
+          "text": "妖怪オクチアナル",
+          "anti": true
+        },
+        {
+          "text": "人生もイージーモードを選択できると思っていた男",
+          "anti": true
+        },
+        {
+          "text": "ガイージー",
+          "anti": true
+        },
+        {
+          "text": "のびハザで義務教育を終えた男",
+          "anti": true
+        },
+        {
+          "text": "のびハザに育てられた男",
+          "anti": true
+        },
+        {
+          "text": "初体験をのびハザで済ませた男",
+          "anti": true
+        },
+        {
+          "text": "のびハザに永久就職した男",
+          "anti": true
+        },
+        {
+          "text": "脳とのびハザを交換された男",
+          "anti": true
+        },
+        {
+          "text": "貝塚のモーツァルト",
+          "anti": true
+        },
+        {
+          "text": "キス待ち無職",
+          "anti": true
+        },
+        {
+          "text": "生きる価値無し",
+          "anti": true
+        },
+        {
+          "text": "意識高い系ガイジ",
+          "anti": true
+        },
+        {
+          "text": "オフ会0人",
+          "anti": true
+        },
+        {
+          "text": "貝塚うんこ製造機",
+          "anti": true
+        },
+        {
+          "text": "クズ",
+          "anti": true
+        },
+        {
+          "text": "糞ウンコ",
+          "anti": true
+        },
+        {
+          "text": "子供おじさん",
+          "anti": true
+        },
+        {
+          "text": "最低野郎",
+          "anti": true
+        },
+        {
+          "text": "終身名誉ホモのおもちゃ",
+          "anti": true
+        },
+        {
+          "text": "セックスむえん",
+          "anti": true
+        },
+        {
+          "text": "精子スプリンクラー",
+          "anti": true
+        },
+        {
+          "text": "底辺YouTuber",
+          "anti": true
+        },
+        {
+          "text": "トロピカル無職",
+          "anti": true
+        },
+        {
+          "text": "難波わいせつ入道",
+          "anti": true
+        },
+        {
+          "text": "ニート",
+          "anti": true
+        },
+        {
+          "text": "能力オールゼロ",
+          "anti": true
+        },
+        {
+          "text": "無職",
+          "anti": true
+        },
+        {
+          "text": "無能",
+          "anti": true
+        },
+        {
+          "text": "モグラ人間",
+          "anti": true
+        },
+        {
+          "text": "ありとあらゆる万事から拒絶された男",
+          "anti": true
+        },
+        {
+          "text": "え？これ人間なの？",
+          "anti": true
+        },
+        {
+          "text": "頭に黒カビが生えている男",
+          "anti": true
+        },
+        {
+          "text": "後ろの襖から忍者が出てきてこいつを連れ去ってほしい",
+          "anti": true
+        },
+        {
+          "text": "いいところが見当たらない男",
+          "anti": true
+        },
+        {
+          "text": "いつも自宅のトイレで迷子になる男",
+          "anti": true
+        },
+        {
+          "text": "回収されたペットボトルの蓋より使えない男",
+          "anti": true
+        },
+        {
+          "text": "貝塚市を財政破綻に追い込んだ男",
+          "anti": true
+        },
+        {
+          "text": "ガキの精神構造のまま体だけ成長してしまった大人",
+          "anti": true
+        },
+        {
+          "text": "神がおふざけで作った男",
+          "anti": true
+        },
+        {
+          "text": "ロックに人権を否定された男",
+          "anti": true
+        },
+        {
+          "text": "リンカーンに奴隷として売られた男",
+          "anti": true
+        },
+        {
+          "text": "六角形のグラフなら限りなく点に近い男",
+          "anti": true
+        },
+        {
+          "text": "心の荒んだ現代社会に現れたオアシス",
+          "anti": false
+        },
+        {
+          "text": "大物Youtuber",
+          "anti": false
+        },
+        {
+          "text": "笑顔をもらえる",
+          "anti": false
+        },
+        {
+          "text": "勇気をもらえる",
+          "anti": false
+        },
+        {
+          "text": "常人にも狂人にもできないことをやってのける男",
+          "anti": false
+        },
+        {
+          "text": "世界一人を笑わせた無職",
+          "anti": false
+        },
+        {
+          "text": "才能の塊",
+          "anti": false
+        },
+        {
+          "text": "伝説となった男",
+          "anti": false
+        },
+        {
+          "text": "最高の無職",
+          "anti": false
+        },
+        {
+          "text": "地球上で最も好かれる男の一人",
+          "anti": false
+        },
+        {
+          "text": "感動をありがとう",
+          "anti": false
+        },
+        {
+          "text": "動画楽しかった！",
+          "anti": false
+        },
+        {
+          "text": "ファンタジーに火をつけた男",
+          "anti": false
+        },
+        {
+          "text": "世界の上にサングラスをかけた男",
+          "anti": false
+        },
+        {
+          "text": "すべてが足りている",
+          "anti": false
+        },
+        {
+          "text": "なんだかんだで愛されている",
+          "anti": false
+        },
+        {
+          "text": "女子10人くらいでいきます",
+          "anti": false
+        },
+        {
+          "text": "おい！人気者！",
+          "anti": false
+        },
+        {
+          "text": "アンチを憎んで人を憎まず",
+          "anti": false
+        },
+        {
+          "text": "ファンが居ないと嘆くよりもネカマを信じて傷つくほうが良い",
+          "anti": false
+        },
+        {
+          "text": "笑顔スプリンクラー",
+          "anti": false
+        },
+        {
+          "text": "ありがとう",
+          "anti": false
+        },
+        {
+          "text": "大ファンです！",
+          "anti": false
+        }
+      ],
+      num_messages: 0,
+      items: [
+        { header: '新着' }
+      ]
+    }
+  },
+  methods: {
+    selectAsSpam: function() {
+
+    },
+    addComment: function() {
+      let index = Math.floor(Math.random() * this.source.length)
+      let index_pic = Math.floor(Math.random() * 6) + 1
+      this.num_messages++
+      this.items.push({
+        name: Math.random().toString(36).slice(-8),
+        avatar: "./icons/anti" + index_pic + ".png",
+        comment: this.source[index].text
+      })
+      if (this.items.length > 6) {
+        this.items.shift()
+      }
+    },
+    update: function() {
+      const parent = this
+      setInterval(function(){
+        parent.addComment()
+      }, 5000)
+    }
+  },
+  mounted: function() {
+    this.update()
+  }
+})
